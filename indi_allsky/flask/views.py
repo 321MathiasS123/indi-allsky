6c662cba0bcff1d1f9f52b1b14bf6f8567492f98
321MathiasS123 <131014304+321MathiasS123@users.noreply.github.com>
2026-09-13 20:30:35 +0200

    Remove abandoned automation API support from capture health

--- indi_allsky/flask/views.py
+++ indi_allsky/flask/views.py
@@ -32,7 +32,10 @@ from ..lens_solver import parseSolverRequestValues
 from ..lens_solver import applySolvedValuesToConfig
 from ..panorama import panoramaSourceCircleClipped
 from ..panorama import validatePanoramaMiniTimelapseRequest
-from ..capture_health import local_capture_health
+from ..capture_health import capture_health_status
+from ..capture_health import capture_stale_seconds
+from ..capture_health import parse_capture_error
+from ..capture_health import CAPTURE_PIPELINE_ERROR_STATE
 
 from cryptography.fernet import InvalidToken
 
@@ -312,7 +315,104 @@ class JsonLatestImageView(JsonView):
 
 
     def getCaptureHealth(self, night):
-        return local_capture_health(self.indi_allsky_config, self.camera, night)
+        now_timestamp = time.time()
+        stale_seconds = capture_stale_seconds(self.indi_allsky_config, night)
+
+        if not self.local_indi_allsky:
+            return capture_health_status(
+                now_timestamp,
+                None,
+                None,
+                stale_seconds,
+                capture_expected=False,
+            )
+
+        capture_expected = True
+        system_error = None
+
+        if self.capture_pause:
+            capture_expected = False
+        elif not night and not self.daytime_capture:
+            capture_expected = False
+
+
+        try:
+            active_camera_id = int(self._miscDb.getState('DB_CAMERA_ID'))
+        except (NoResultFound, TypeError, ValueError):
+            active_camera_id = None
+
+        if active_camera_id is not None and active_camera_id != self.camera.id:
+            return capture_health_status(
+                now_timestamp,
+                None,
+                None,
+                stale_seconds,
+                capture_expected=False,
+            )
+
+
+        try:
+            capture_status = int(self._miscDb.getState('STATUS'))
+        except (NoResultFound, TypeError, ValueError):
+            capture_status = None
+
+        status_error_map = {
+            constants.STATUS_NOCAMERA: 'Capture unavailable: no camera detected.',
+            constants.STATUS_CAMERAERROR: 'Capture unavailable: camera error.',
+            constants.STATUS_NOINDISERVER: 'Capture unavailable: INDI server is not running.',
+        }
+
+        if capture_status in status_error_map and capture_expected:
+            system_error = status_error_map[capture_status]
+        elif capture_status is not None and capture_status != constants.STATUS_RUNNING:
+            capture_expected = False
+
+
+        latest_success_timestamp = None
+
+        image_dir = Path(self.indi_allsky_config['IMAGE_FOLDER']).absolute()
+        latest_image_p = image_dir.joinpath(
+            'latest.{0:s}'.format(
+                self.indi_allsky_config.get('IMAGE_FILE_TYPE', 'jpg'),
+            )
+        )
+
+        try:
+            latest_success_timestamp = latest_image_p.stat().st_mtime
+        except FileNotFoundError:
+            pass
+        except OSError as e:
+            app.logger.error('Unable to stat latest image: %s', str(e))
+
+        if latest_success_timestamp is None:
+            latest_image_date = db.session.query(
+                IndiAllSkyDbImageTable.createDate,
+            )\
+                .join(IndiAllSkyDbImageTable.camera)\
+                .filter(IndiAllSkyDbCameraTable.id == self.camera.id)\
+                .order_by(IndiAllSkyDbImageTable.createDate.desc())\
+                .first()
+
+            if latest_image_date:
+                latest_success_timestamp = latest_image_date[0].timestamp()
+
+
+        try:
+            failure = parse_capture_error(
+                self._miscDb.getState(CAPTURE_PIPELINE_ERROR_STATE),
+            )
+        except NoResultFound:
+            failure = None
+
+
+        return capture_health_status(
+            now_timestamp,
+            latest_success_timestamp,
+            failure,
+            stale_seconds,
+            capture_expected=capture_expected,
+            system_error=system_error,
+        )
 
 
     def get_objects(self):
@@ -14913,3 +15013,4 @@ def manifest():
     response = jsonify(manifest_data)
     response.headers['Content-Type'] = 'application/manifest+json'
     return response
+
